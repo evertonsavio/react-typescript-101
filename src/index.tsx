1ce75a76906f2fcf9c2c1580835f45ada88ee857
Rhavyx <everluca@outlook.com>
2021-08-19 09:36:09 -0300

    :sparkles: change event

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom';
+import EventComponent from './events/eventComponent';
 import GuestList from './state/guestList';
 import UserSearch from './state/userSearch';
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
         <GuestList />
         <UserSearch />
+        <EventComponent />
     </div>
     )
 }
